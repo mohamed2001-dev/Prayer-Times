29e2ad21358d24180298b3e047ee12487f30b259
mohamed <amalkirifi01@gmail.com>
2026-05-20 15:40:14 +0200

    add ui fir app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
-
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import {fetchPrayerTimes} from "./slices/prayerTimesSlice"
 function App() {
+  const dispatch = useDispatch()
+  const {loading , error , timings } = useSelector((state)=> state.prayerTimes)
+
+  useEffect(()=>{
+    dispatch(fetchPrayerTimes())
+  },[dispatch])
 
 
   return (
-    <>
-      <div>
-        <h1 className="text-black font-bold flex justify-center mt-5 text-5xl">Prayer Times</h1>
-      </div>
-    </>
+     <div>
+      <h1 className="flex justify-center text-5xl font-bold mt-5">Prayer Times</h1>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
+
+      {timings && Object.entries(timings).map(([name, time]) => (
+        <div
+          key={name}
+          className="flex justify-around"
+        >
+          <h2 className="">{name}: {time}</h2>
+        </div>
+      ))}
+    </div>
   )
 }
 
--- src/slices/prayerTimesSlice.js
+++ src/slices/prayerTimesSlice.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const fetchPrayerTimes = createAsyncThunk("prayerTimes",async(_ , {rejectWithValue})=>{
+export const fetchPrayerTimes = createAsyncThunk("prayerTimes",async(_ , {rejectWithValue})=>{
     try {
         const response = await axios.get("https://api.aladhan.com/v1/timings?latitude=33.5731&longitude=-7.5898&method=3")
         return response.data
